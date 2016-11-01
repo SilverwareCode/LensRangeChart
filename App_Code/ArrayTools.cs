e36ac5f6c591da516ae622070d0e168b62b536da
SilverwearCode <matous.havranek@gmail.com>
2016-11-01 11:49:17 +0100

    Classes for JSON generation added

---
+++ App_Code/ArrayTools.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Web;
+
+/// <summary>
+/// Summary description for WebSystemArray
+/// </summa
+namespace WebSystem
+{
+    public class ArrayTools
+    {
+        public static Array ResizeArray(Array arr, int[] newSizes)
+        {
+            //funkce redimenzuje dvojrozmerne pole
+            //volani funkce napr. myGroup.lens = (Lens[,])WebSystem.ArrayTools.ResizeArray(myGroup.lens, new int[] { 12, 2 });
+
+            if (newSizes.Length != arr.Rank)
+                throw new ArgumentException("arr must have the same number of dimensions " +
+                                            "as there are elements in newSizes", "newSizes");
+
+            var temp = Array.CreateInstance(arr.GetType().GetElementType(), newSizes);
+            int length = arr.Length <= temp.Length ? arr.Length : temp.Length;
+            Array.ConstrainedCopy(arr, 0, temp, 0, length);
+            return temp;
+        }
+
+
+    }
+}
---
+++ App_Code/LensRange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// trida popisuje dostupnost cocek, tzv. piskvorky
+/// </summary>
+
+namespace YO
+{
+    public class LensGroup
+    {
+        public int id;//identifikator skupiny
+        public string name;//jmeno skupiny
+        public float diameter; //prumer cocek ve skupine
+        public Lens[,] lens = new Lens[1, 1];
+    }
+
+    public class Lens
+    {
+        public string OPC;//unikatni kod pro kazdou cocku
+        public float Cyl;//hodnota Cylindru cocky
+        public float Sph;//hodnota Sfery cocky
+        public float Base;//hodnota baze cocky
+        public float Add;//hodnota Adice cocky
+        public int Eye;//udava jestli je cocka pro prave, leve nebo obe oci
+        public float Price;//cena za cocku
+    }
+
+}
+
